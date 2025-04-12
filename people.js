89d1db50694295848cab9e01aaacae3facd908c2
Katie Bishop <katiebishop207@gmail.com>
2025-04-12 10:55:36 -0600

    default movies, people, tv pages

--- people.js
+++ people.js
@@ -8,19 +8,50 @@ const navButton = document.getElementById("nav-button");
 const drawer = document.getElementById("drawer")
 let drawerIsOpen = false;
 
-
 // Nav drawer open and close
 navButton.addEventListener("click", e => {
     drawerIsOpen = !drawerIsOpen;
     drawer.dataset.open = `${drawerIsOpen}`
 });
 
-// Navigate with a query String
-findButton.addEventListener("click", e => {
-    e.preventDefault();
-    console.log(`${e.target.href}?id=${idInput.value}`);
-    window.location.href = `${e.target.href}?id=${idInput.value}`;
-});
+// // Navigate with a query String
+// findButton.addEventListener("click", e => {
+//     e.preventDefault();
+//     console.log(`${e.target.href}?id=${idInput.value}`);
+//     window.location.href = `${e.target.href}?id=${idInput.value}`;
+// });
+
+const number_of_cards = 50;
+
+peoplePopular()
+    .then(results => {
+        for (let i = 0; i < number_of_cards; i++) { // iterate over number of cards
+            // create card with info
+            console.log(results.results[i])
+            let img = document.createElement('img')
+            let card = document.createElement('div')
+            let name = document.createElement('h3')
+
+            img.src = `${imgUrl}w500${results.results[i].profile_path}`
+            img.alt = `${results.results[i].name} profile picture`
+            name.innerText = `${results.results[i].name}`
+
+            card.appendChild(img);
+            card.appendChild(name);
+            card.className = "info-card"
+            card.id = `person-card-${i}`
+
+            card.addEventListener("click", (e) => {
+                e.preventDefault()
+                window.location.href = `person.html?id=${results.results[i].id}`;
+
+            })
+
+            document.getElementById("person-card-container").appendChild(card)
+        }
+    })
+    .catch(error => console.log(error));
+
 
 // Make the call to get results from the search
 peopleSearch(peopleName)
--- tv.js
+++ tv.js
@@ -23,6 +23,39 @@ navButton.addEventListener("click", e => {
 //     window.location.href = `${e.target.href}?id=${idInput.value}`;
 // });
 
+const number_of_cards = 50;
+
+tvPopular()
+    .then(results => {
+        console.log(results, "results")
+        for (let i = 0; i < number_of_cards; i++) { // iterate over number of cards
+            // create card with info
+            let img = document.createElement('img')
+            let card = document.createElement('div')
+            let title = document.createElement('h3')
+            let voteAverage = document.createElement('div')
+
+            img.src = `${imgUrl}w500${results.results[i].poster_path}`
+            img.alt = `${results.results[i].name} poster`
+            title.innerText = `${results.results[i].name} (${results.results[i].first_air_date.slice(0,4)})`
+            voteAverage.innerText = `Vote Average: ${results.results[i].vote_average}`
+
+            card.appendChild(img);
+            card.appendChild(title);
+            card.appendChild(voteAverage);
+            card.className = "info-card"
+            card.id = `tv-card-${i}`
+
+            card.addEventListener("click", (e) => {
+                e.preventDefault()
+                window.location.href = `series.html?id=${results.results[i].id}`;
+
+            })
+
+            document.getElementById("tv-card-container").appendChild(card)
+        }
+    })
+    .catch(error => console.log(error));
 
 
 
